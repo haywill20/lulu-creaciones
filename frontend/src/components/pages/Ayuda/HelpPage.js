8cfd5160d6d04a6d53e92c29b780833aa97afc90
haywill20 <mirandacaceresm6@gmail.com>
2023-10-06 17:49:45 -0600

    se creo el metodo para insertar los datos del formulario de ayuda desde frontend

--- frontend/src/components/pages/Ayuda/HelpPage.js
+++ frontend/src/components/pages/Ayuda/HelpPage.js
@@ -1,9 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import Footer from "../../common/Footer";
 import Header from "../../common/Header";
 import TopBar from "../../common/TopBar";
+import axios from "axios";
+
+const URI = "http://localhost:8000/ayudas/";
 
 const HelpPage = () => {
+  const [nombre, setNombre] = useState("");
+  const [correo, setCorreo] = useState("");
+  const [mensaje, setMensaje] = useState("");
+  const [showModal, setShowModal] = useState(false);
+
+  const ayuda = async (e) => {
+    e.preventDefault();
+
+    await axios.post(URI, {
+      nombre: nombre,
+      correo: correo,
+      mensaje: mensaje,
+    });
+
+    // Mostrar la ventana modal
+    setShowModal(true);
+
+    // Limpiar los campos del formulario
+    setNombre("");
+    setCorreo("");
+    setMensaje("");
+  };
+
+  const closeModal = () => {
+    setShowModal(false);
+  };
   return (
     <>
       <TopBar />
@@ -24,7 +53,7 @@ const HelpPage = () => {
                   id="main-contact-form"
                   className="contact-form row"
                   name="contact-form"
-                  method="post"
+                  onSubmit={ayuda}
                 >
                   <div className="form-group col-md-12">
                     <div className="col-sm-6">
@@ -34,6 +63,8 @@ const HelpPage = () => {
                         className="form-control"
                         required="required"
                         placeholder="Nombre"
+                        value={nombre}
+                        onChange={(e) => setNombre(e.target.value)}
                       />
                     </div>
                     <div className="col-sm-6">
@@ -43,6 +74,8 @@ const HelpPage = () => {
                         className="form-control"
                         required="required"
                         placeholder="correo"
+                        value={correo}
+                        onChange={(e) => setCorreo(e.target.value)}
                       />
                     </div>
                   </div>
@@ -57,6 +90,8 @@ const HelpPage = () => {
                         className="form-control"
                         rows="8"
                         placeholder="Escriba su mensaje aqui"
+                        value={mensaje}
+                        onChange={(e) => setMensaje(e.target.value)}
                       ></textarea>
                     </div>
                   </div>
@@ -70,6 +105,48 @@ const HelpPage = () => {
                     />
                   </div>
                 </form>
+              </div>
+              <div
+                className={`modal fade ${showModal ? "in" : ""}`}
+                id="successModal"
+                role="dialog"
+                style={{ display: showModal ? "block" : "none" }}
+              >
+                <div className="modal-dialog">
+                  <div className="modal-content">
+                    <div className="modal-header">
+                      <button
+                        type="button"
+                        className="close"
+                        onClick={closeModal}
+                      >
+                        &times;
+                      </button>
+                      <h4 className="modal-title">¡Enviado exitosamente!</h4>
+                    </div>
+                    <div className="modal-body">
+                      <p>
+                        ¡Gracias por ponerte en con el soporte al cliente de
+                        Lúlu creaciones!
+                        <br />
+                        Pronto nos podremos en contacto contigo para darte
+                        soporte{" "}
+                        <span>
+                          <i class="fa-regular fa-thumbs-up"></i>
+                        </span>
+                      </p>
+                    </div>
+                    <div className="modal-footer">
+                      <button
+                        type="button"
+                        className="btn btn-default"
+                        onClick={closeModal}
+                      >
+                        Cerrar
+                      </button>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
